d1f90bda45c46f72903288c792ed84bce1fa5069
Amina <tktrv.amina@gmail.com>
2026-04-25 17:15:10 +0500

    add all enums

---
+++ University/src/enums/Format.java
@@ -0,0 +1,6 @@
+package enums;
+
+public enum Format {
+    PLAIN_TEXT,
+    BIBTEX
+}
---
+++ University/src/enums/NewsTopic.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum NewsTopic {
+    RESEARCH,
+    ACADEMIC,
+    EVENT,
+    ANNOUNCEMENT
+}
