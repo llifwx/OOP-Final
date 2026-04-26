b80e72977a591f0b74d8b66b0543e2d0b1819509
bzrcerk <lnikbzr@gmail.com>
2026-04-26 22:02:06 +0500

    feat/add-class-techsupport

---
+++ University/src/model/users/TechSupportSpecialist.java
@@ -0,0 +1,28 @@
+package model.users;
+
+import enums.Language;
+import support.TechSupportReq;
+
+import java.util.Date;
+import java.util.List;
+
+public class TechSupportSpecialist extends Employee{
+    private List<TechSupportReq> requests;
+
+    public TechSupportSpecialist(String username, String password, String fullName, String email, Language language, String employeeId, String department, double salary, Date hierDate, List<Message> inbox, List<TechSupportReq> requests) {
+        super(username, password, fullName, email, language, employeeId, department, salary, hierDate, inbox);
+        this.requests = requests;
+    }
+
+    public List<TechSupportReq> getRequests() {return requests;}
+
+    public List<TechSupportReq> viewNewRequests() {}
+    public void acceptRequest(TechSupportReq req) {}
+    public void rejectRequest(TechSupportReq req) {}
+    public void markAsDone(TechSupportReq req) {}
+
+    @Override
+    public String toString() {
+        return "Requests: " + this.getRequests();
+    }
+}
